82d8941122e4355a4a07862313db479b2455f8a2
Joao Pedro <joaopedreleonel@gmail.com>
2025-12-03 00:40:02 -0300

    inicial project stup

---
+++ AgroHack/AgroHack/AgroHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgroHackApp.swift
+//  AgroHack
+//
+//  Created by Joao pedro Leonel on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AgroHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
